e7031ee82b1a224ec9887a554e33ed73bc081f3c
Amir <hesamian@uwm.edu>
2020-03-15 14:52:42 -0500

    Not sure how to get it working on mac

--- Api/wwwroot/js/app.jsx
+++ Api/wwwroot/js/app.jsx
@@ -1,3 +1,39 @@
+function recordPolyfill(onGetUserMedia, onGetUserMediaError) {
+    // from: https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+    // Older browsers might not implement mediaDevices at all, so we set an empty object first
+    if (navigator.mediaDevices === undefined) {
+        navigator.mediaDevices = {};
+    }
+
+    // Some browsers partially implement mediaDevices. We can't just assign an object
+    // with getUserMedia as it would overwrite existing properties.
+    // Here, we will just add the getUserMedia property if it's missing.
+    if (navigator.mediaDevices.getUserMedia === undefined) {
+        navigator.mediaDevices.getUserMedia = constraints => {
+
+            // First get a hold of the legacy getUserMedia, if present
+            let getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+            // Some browsers just don't implement it - return a rejected promise with an error
+            // to keep a consistent interface
+            if (!getUserMedia) {
+                return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+            }
+
+            // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+            return new Promise((resolve, reject) => {
+                getUserMedia.call(navigator, constraints, resolve, reject);
+            });
+        };
+    }
+
+    if (navigator.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({audio: true})
+            .then(onGetUserMedia)
+            .catch(onGetUserMediaError);
+    }
+}
+
 class BasicComponent extends React.Component {
 
     handleSubmit = submitHandler => event => {
@@ -54,31 +90,32 @@ class Messenger extends BasicComponent {
         };
     }
 
-    recordToggle = callback => async () => {
+    recordToggle = callback => () => {
         if (this.state.recording) {
             this.state.recorder.stop();
             this.state.stream.getAudioTracks()[0].stop();
             this.setState({stream: null, recorder: null, recording: false});
         } else {
-            let stream = await navigator.mediaDevices.getUserMedia({audio: true});
-            let recorder = new MediaRecorder(stream);
+            recordPolyfill(async stream => {
+                let recorder = new MediaRecorder(stream);
 
-            await this.setState({stream, recorder, recording: true});
+                await this.setState({stream, recorder, recording: true});
 
-            recorder.ondataavailable = async e => {
-                let blob = e.data;
-                await this.setState({recording: false});
+                recorder.ondataavailable = async e => {
+                    let blob = e.data;
+                    await this.setState({recording: false});
 
-                blobUtil.blobToBase64String(blob).then((base64String) => {
-                    // success
-                    callback(base64String);
-                }).catch((err) => {
-                    // error
-                    console.error(err);
-                });
-            };
+                    blobUtil.blobToBase64String(blob).then((base64String) => {
+                        // success
+                        callback(base64String);
+                    }).catch((err) => {
+                        // error
+                        console.error(err);
+                    });
+                };
 
-            recorder.start();
+                recorder.start();
+            }, err => console.error(err));
         }
     };
 
